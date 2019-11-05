9d5f0cf7eb5416150768353220426374790dab24
林岑影 <lincenying@qq.com>
2019-11-05 11:37:45 +0800

    no message

--- build/build.js
+++ build/build.js
@@ -16,6 +16,7 @@ cp('-R', 'favicon.ico', relative + '/dist/')
 
 webpack(config).run(function(err, stats) {
     if (err) {
+        console.log(err);
         console.error('Failed to create a production build. Reason:')
         console.error(err.message || err)
         process.exit(1)
